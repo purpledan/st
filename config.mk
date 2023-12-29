559364a3a85b1db1fb5798f0b3f9f607c35a6e4b
Dan <221143711@student.uj.ac.za>
2023-12-29 13:53:14 +0200

    FreeBSD make patch

--- config.mk
+++ config.mk
@@ -5,10 +5,10 @@ VERSION = 0.9
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MANPREFIX = $(PREFIX)/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = ${PREFIX}/include
+X11LIB = ${PREFIX}/lib
 
 PKG_CONFIG = pkg-config
 
